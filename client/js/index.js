e17471f09b80e6ffa86b74547c0bdd5d035e6808
aleda <alessandro.dolce001@studenti.uniparthenope.it>
2023-01-09 14:20:35 +0100

    Changed the functionality of the "GET STARTED" button: when you are not logged in, the button takes you to the Log In page. When you are logged in, the button takes you to the questionnaire page.

--- client/js/index.js
+++ client/js/index.js
@@ -1,15 +1,18 @@
 import { API_URL } from "./config.js"
 
+//COMMENT
 fetch(`${API_URL}/userDetails`, {
     method: "GET",
     credentials: "include"
 }).then(response => {
     console.log(response.status)
     if (!response.ok) {
-        document.getElementById("login-navbar").textContent = "Log in"
+        document.getElementById("login-navbar").textContent = "Log In"
         document.getElementById("login-navbar").setAttribute('href', 'login.html')
     } else {
-        document.getElementById("login-navbar").textContent = "Logout"
+        document.getElementById("login-navbar").textContent = "Log Out"
         document.getElementById("login-navbar").setAttribute('href', 'logout.html')
+
+        document.getElementById("getstarted_button").setAttribute('href', 'questionary.html')
     }
 })
